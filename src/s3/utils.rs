0fb80e1456dcb9f73a7b31437c35647966f247fe
Bala FA <bala.gluster@gmail.com>
2022-08-21 03:40:11 +0530

    Refactor S3 client implementation (#13)
    Added below S3 APIs
    
    * abort_multipart_upload()
    * bucket_exists()
    * complete_multipart_upload()
    * create_multipart_upload()
    * get_object()
    * list_buckets()
    * list_objects_v1()
    * list_objects_v2()
    * list_object_versions()
    * list_objects()
    * make_bucket()
    * put_object()
    * put_object_api()
    * remove_bucket()
    * remove_object()
    * remove_objects_api()
    * remove_objects()
    * select_object_content()
    * stat_object()
    * upload_part()
    
    Signed-off-by: Bala.FA <bala@minio.io>

---
+++ src/s3/utils.rs
@@ -0,0 +1,282 @@
+// MinIO Rust Library for Amazon S3 Compatible Cloud Storage
+// Copyright 2022 MinIO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::s3::error::Error;
+pub use base64::encode as b64encode;
+use byteorder::{BigEndian, ReadBytesExt};
+use chrono::{DateTime, NaiveDateTime, ParseError, Utc};
+use chrono_locale::LocaleDate;
+use crc::{Crc, CRC_32_ISO_HDLC};
+use lazy_static::lazy_static;
+use md5::compute as md5compute;
+use multimap::MultiMap;
+use regex::Regex;
+use sha2::{Digest, Sha256};
+pub use urlencoding::decode as urldecode;
+pub use urlencoding::encode as urlencode;
+use xmltree::Element;
+
+pub type UtcTime = DateTime<Utc>;
+
+pub type Multimap = MultiMap<String, String>;
+
+pub fn merge(m1: &mut Multimap, m2: &Multimap) {
+    for (key, values) in m2.iter_all() {
+        for value in values {
+            m1.insert(key.to_string(), value.to_string());
+        }
+    }
+}
+
+pub fn crc32(data: &[u8]) -> u32 {
+    Crc::<u32>::new(&CRC_32_ISO_HDLC).checksum(data)
+}
+
+pub fn uint32(mut data: &[u8]) -> Result<u32, std::io::Error> {
+    data.read_u32::<BigEndian>()
+}
+
+pub fn sha256_hash(data: &[u8]) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(data);
+    return format!("{:x}", hasher.finalize());
+}
+
+pub fn md5sum_hash(data: &[u8]) -> String {
+    b64encode(md5compute(data).as_slice())
+}
+
+pub fn utc_now() -> UtcTime {
+    chrono::offset::Utc::now()
+}
+
+pub fn to_signer_date(time: UtcTime) -> String {
+    time.format("%Y%m%d").to_string()
+}
+
+pub fn to_amz_date(time: UtcTime) -> String {
+    time.format("%Y%m%dT%H%M%SZ").to_string()
+}
+
+pub fn to_http_header_value(time: UtcTime) -> String {
+    time.formatl("%a, %d %b %Y %H:%M:%S GMT", "C").to_string()
+}
+
+pub fn to_iso8601utc(time: UtcTime) -> String {
+    time.format("%Y-%m-%dT%H:%M:%S.%3fZ").to_string()
+}
+
+pub fn from_iso8601utc(s: &str) -> Result<UtcTime, ParseError> {
+    Ok(DateTime::<Utc>::from_utc(
+        match NaiveDateTime::parse_from_str(s, "%Y-%m-%dT%H:%M:%S.%3fZ") {
+            Ok(d) => d,
+            _ => NaiveDateTime::parse_from_str(s, "%Y-%m-%dT%H:%M:%SZ")?,
+        },
+        Utc,
+    ))
+}
+
+pub fn from_http_header_value(s: &str) -> Result<UtcTime, ParseError> {
+    Ok(DateTime::<Utc>::from_utc(
+        NaiveDateTime::parse_from_str(s, "%a, %d %b %Y %H:%M:%S GMT")?,
+        Utc,
+    ))
+}
+
+pub fn to_http_headers(map: &Multimap) -> Vec<String> {
+    let mut headers: Vec<String> = Vec::new();
+    for (key, values) in map.iter_all() {
+        for value in values {
+            let mut s = String::new();
+            s.push_str(key);
+            s.push_str(": ");
+            s.push_str(value);
+            headers.push(s);
+        }
+    }
+    return headers;
+}
+
+pub fn to_query_string(map: &Multimap) -> String {
+    let mut query = String::new();
+    for (key, values) in map.iter_all() {
+        for value in values {
+            if !query.is_empty() {
+                query.push_str("&");
+            }
+            query.push_str(&urlencode(key));
+            query.push_str("=");
+            query.push_str(&urlencode(value));
+        }
+    }
+    return query;
+}
+
+pub fn get_canonical_query_string(map: &Multimap) -> String {
+    let mut keys: Vec<String> = Vec::new();
+    for (key, _) in map.iter() {
+        keys.push(key.to_string());
+    }
+    keys.sort();
+
+    let mut query = String::new();
+    for key in keys {
+        match map.get_vec(key.as_str()) {
+            Some(values) => {
+                for value in values {
+                    if !query.is_empty() {
+                        query.push_str("&");
+                    }
+                    query.push_str(&urlencode(key.as_str()));
+                    query.push_str("=");
+                    query.push_str(&urlencode(value));
+                }
+            }
+            None => todo!(), // This never happens.
+        };
+    }
+
+    return query;
+}
+
+pub fn get_canonical_headers(map: &Multimap) -> (String, String) {
+    lazy_static! {
+        static ref MULTI_SPACE_REGEX: Regex = Regex::new("( +)").unwrap();
+    }
+    let mut signed_headers: Vec<String> = Vec::new();
+    let mut mmap: MultiMap<String, String> = MultiMap::new();
+
+    for (k, values) in map.iter_all() {
+        let key = k.to_lowercase();
+        if "authorization" == key || "user-agent" == key {
+            continue;
+        }
+        if !signed_headers.contains(&key) {
+            signed_headers.push(key.clone());
+        }
+
+        for v in values {
+            mmap.insert(key.clone(), v.to_string());
+        }
+    }
+
+    let mut canonical_headers: Vec<String> = Vec::new();
+    for (key, values) in mmap.iter_all_mut() {
+        values.sort();
+        let mut value = String::new();
+        for v in values {
+            if !value.is_empty() {
+                value.push_str(",");
+            }
+            let s: String = MULTI_SPACE_REGEX.replace_all(v, " ").to_string();
+            value.push_str(&s);
+        }
+        canonical_headers.push(key.to_string() + ":" + value.as_str());
+    }
+
+    signed_headers.sort();
+    canonical_headers.sort();
+
+    return (signed_headers.join(";"), canonical_headers.join("\n"));
+}
+
+pub fn check_bucket_name(bucket_name: &str, strict: bool) -> Result<(), Error> {
+    if bucket_name.trim().is_empty() {
+        return Err(Error::InvalidBucketName(String::from(
+            "bucket name cannot be empty",
+        )));
+    }
+
+    if bucket_name.len() < 3 {
+        return Err(Error::InvalidBucketName(String::from(
+            "bucket name cannot be less than 3 characters",
+        )));
+    }
+
+    if bucket_name.len() > 63 {
+        return Err(Error::InvalidBucketName(String::from(
+            "Bucket name cannot be greater than 63 characters",
+        )));
+    }
+
+    lazy_static! {
+        static ref VALID_IP_ADDR_REGEX: Regex = Regex::new("^(\\d+\\.){3}\\d+$").unwrap();
+        static ref VALID_BUCKET_NAME_REGEX: Regex =
+            Regex::new("^[A-Za-z0-9][A-Za-z0-9\\.\\-_:]{1,61}[A-Za-z0-9]$").unwrap();
+        static ref VALID_BUCKET_NAME_STRICT_REGEX: Regex =
+            Regex::new("^[a-z0-9][a-z0-9\\.\\-]{1,61}[a-z0-9]$").unwrap();
+    }
+
+    if VALID_IP_ADDR_REGEX.is_match(bucket_name) {
+        return Err(Error::InvalidBucketName(String::from(
+            "bucket name cannot be an IP address",
+        )));
+    }
+
+    if bucket_name.contains("..") || bucket_name.contains(".-") || bucket_name.contains("-.") {
+        return Err(Error::InvalidBucketName(String::from(
+            "bucket name contains invalid successive characters '..', '.-' or '-.'",
+        )));
+    }
+
+    if strict {
+        if !VALID_BUCKET_NAME_STRICT_REGEX.is_match(bucket_name) {
+            return Err(Error::InvalidBucketName(String::from(
+                "bucket name does not follow S3 standards strictly",
+            )));
+        }
+    } else if !VALID_BUCKET_NAME_REGEX.is_match(bucket_name) {
+        return Err(Error::InvalidBucketName(String::from(
+            "bucket name does not follow S3 standards",
+        )));
+    }
+
+    return Ok(());
+}
+
+pub fn get_text(element: &Element, tag: &str) -> Result<String, Error> {
+    Ok(element
+        .get_child(tag)
+        .ok_or(Error::XmlError(format!("<{}> tag not found", tag)))?
+        .get_text()
+        .ok_or(Error::XmlError(format!("text of <{}> tag not found", tag)))?
+        .to_string())
+}
+
+pub fn get_option_text(element: &Element, tag: &str) -> Result<Option<String>, Error> {
+    Ok(match element.get_child(tag) {
+        Some(v) => Some(
+            v.get_text()
+                .ok_or(Error::XmlError(format!("text of <{}> tag not found", tag)))?
+                .to_string(),
+        ),
+        None => None,
+    })
+}
+
+pub fn get_default_text(element: &Element, tag: &str) -> String {
+    element.get_child(tag).map_or(String::new(), |v| {
+        v.get_text().unwrap_or_default().to_string()
+    })
+}
+
+pub fn copy_slice(dst: &mut [u8], src: &[u8]) -> usize {
+    let mut c = 0;
+    for (d, s) in dst.iter_mut().zip(src.iter()) {
+        *d = *s;
+        c += 1;
+    }
+    c
+}
