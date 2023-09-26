e5f6b1605186aeff51b8c34320b037d90babebe4
Aditya Manthramurthy <donatello@users.noreply.github.com>
2023-09-26 10:43:05 -0700

    Add file-uploader example (#49)
    Also add some basic fields to Cargo.toml to prepare for initial release.

--- examples/file-uploader.rs
+++ examples/file-uploader.rs
@@ -1,11 +1,3 @@
-# MinIO Rust SDK for Amazon S3 Compatible Cloud Storage [![Slack](https://slack.min.io/slack?type=svg)](https://slack.min.io) [![Sourcegraph](https://sourcegraph.com/github.com/minio/minio-rs/-/badge.svg)](https://sourcegraph.com/github.com/minio/minio-rs?badge) [![Apache V2 License](https://img.shields.io/badge/license-Apache%20V2-blue.svg)](https://github.com/minio/minio-rs/blob/master/LICENSE)
-
-MinIO Rust SDK is Simple Storage Service (aka S3) client to perform bucket and object operations to any Amazon S3 compatible object storage service.
-
-For a complete list of APIs and examples, please take a look at the [MinIO Rust Client API Reference](https://minio-rs.min.io/)
-
-## Example:: file-uploader.rs
-```rust
 use minio::s3::args::{BucketExistsArgs, MakeBucketArgs, UploadObjectArgs};
 use minio::s3::client::Client;
 use minio::s3::creds::StaticProvider;
@@ -62,7 +54,3 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     println!("'/home/user/Photos/asiaphotos.zip' is successfully uploaded as object 'asiaphotos-2015.zip' to bucket 'asiatrip'.");
     Ok(())
 }
-```
-
-## License
-This SDK is distributed under the [Apache License, Version 2.0](https://www.apache.org/licenses/LICENSE-2.0), see [LICENSE](https://github.com/minio/minio-rs/blob/master/LICENSE) for more information.
